8cd649a125fa7d155ef6b9924253189e298e8868
Joey Cheung <joeycheung0@gmail.com>
2018-04-13 00:53:47 -0400

    Changed files

--- TicTacToe.java
+++
@@ -1,9 +0,0 @@
-
-public class TicTacToe {
-
-	public static void main (String[] args) {
-		
-		TicTacToeGUI gui = new TicTacToeGUI();
-		gui.initialize();
-	}
-}
---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,9 @@
+
+public class TicTacToe {
+
+	public static void main (String[] args) {
+		
+		TicTacToeGUI gui = new TicTacToeGUI();
+		gui.initialize();
+	}
+}
